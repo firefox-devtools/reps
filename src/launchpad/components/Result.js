84560716473d92ab7afcd779ed134dc24702ae8f
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2016-12-21 20:17:57 +0100

    Add a way to display and copy packet

--- src/launchpad/components/Result.js
+++ src/launchpad/components/Result.js
@@ -12,6 +12,29 @@ const Result = React.createClass({
 
   displayName: "Result",
 
+  getInitialState: function() {
+    return {
+      showPacket: false
+    };
+  },
+
+  copyPacketToClipboard: function(e, packet) {
+    e.stopPropagation();
+
+    var textField = document.createElement('textarea');
+    textField.innerHTML = JSON.stringify(packet, null, "  ");
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    textField.remove();
+  },
+
+  onHeaderClick: function() {
+    this.setState(prevState => ({
+      showPacket: !prevState || !prevState.showPacket
+    }));
+  },
+
   renderRepInAllModes: function({ object }) {
     return Object.keys(MODE).map(modeKey =>
        this.renderRep({ object, modeKey })
@@ -29,18 +52,38 @@ const Result = React.createClass({
     );
   },
 
+  renderPacket: function(packet) {
+    let showPacket = this.state && this.state.showPacket === true;
+    let headerClassName = showPacket ? "packet-expanded" : "packet-collapsed";
+    let headerLabel = showPacket ? "Hide expression packet" : "Show expression packet";
+
+    return dom.div({ className: "packet" },
+      dom.header({
+          className: headerClassName,
+          onClick: this.onHeaderClick,
+        },
+        headerLabel,
+        showPacket && dom.button({
+          className: "copy-packet-button",
+          onClick: (e) => this.copyPacketToClipboard(e, packet)
+        }, "Copy as JSON")
+      ),
+      showPacket &&
+        dom.div({className: "packet-rep"}, Rep({object: packet}))
+    )
+  },
+
   render: function() {
     let {expression} = this.props;
     let {input, packet} = expression;
-    return dom.div(
-      {
-        className: "rep-row",
-        key: JSON.stringify(expression)
+    return dom.div({
+        className: "rep-row"
       },
       dom.div({ className: "rep-input" }, input),
       dom.div({ className: "reps" }, this.renderRepInAllModes({
         object: packet.exception || packet.result
-      }))
+      })),
+      this.renderPacket(packet)
     );
   }
 });
