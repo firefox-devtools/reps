c03b63b60685005b197c091b703a55b21ab3da50
Jason Laster <jason.laster.11@gmail.com>
2017-01-10 13:10:44 -0500

    remove comments

--- bin/publish-assets.js
+++ bin/publish-assets.js
@@ -6,12 +6,6 @@ function start() {
   const projectPath = path.resolve(__dirname, "..");
   const mcModulePath = "devtools/client/debugger/new";
 
-  // copyFile(
-  //   path.resolve(projectPath, "assets/locales/debugger.properties"),
-  //   path.resolve(projectPath, "assets/build/debugger.properties"),
-  //   {cwd: projectPath}
-  // );
-
   copyFile(
     path.resolve(projectPath, "src/test/mochitest"),
     path.resolve(projectPath, "assets/build"),
