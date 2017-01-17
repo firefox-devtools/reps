60c2bb5444e6dcaf8ed0cd3595447462f8596bd8
Julian Descottes <julian.descottes@gmail.com>
2017-01-17 17:41:54 +0100

    add missing exports to src/index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const React = require("react");
 
 const { MODE } = require("./reps/constants");
 const Rep = React.createFactory(require("./reps/rep"));
+const StringRep = React.createFactory(require("./reps/string").rep);
 const Grip = require("./reps/grip");
+const { createFactories, parseURLEncodedText, parseURLParams } = require("./reps/rep-utils");
 
 module.exports = {
   Rep,
+  StringRep,
   Grip,
-  MODE
+  MODE,
+  createFactories,
+  parseURLEncodedText,
+  parseURLParams,
 };
